8b0ac5482ec9e45ccde31af8add7a7fa3d05104f
cavinHuang <943866180@qq.com>
2023-03-08 20:36:44 +0800

    feat: 增加文件

--- internal/build/constants.ts
+++ internal/build/constants.ts
@@ -6,3 +6,7 @@ import path from 'path'
 export const root = path.resolve(__dirname, '../../')
 
 export const packages = path.resolve(root, './packages')
+
+export const componentPath = path.join(packages, 'components')
+
+export const shared = path.join(packages, 'shared')
